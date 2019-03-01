ee2123c28cfc663691297fcbb7f12b5ded92ce61
wansho <wanshojs@hotmail.com>
2019-03-01 19:00:19 +0800

    add github url

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     description = "submit url to baidu.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/pypa/sampleproject",
+    url = "https://github.com/wansho/baidu-url-submit",
     packages = setuptools.find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
